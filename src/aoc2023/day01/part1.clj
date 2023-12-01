c8089e1c57631ba924982e714f640a671961a185
Stefan Filev <stefan.s.filev@gmail.com>
2023-12-01 15:25:05 +0000

    solution for part1 day01

--- src/aoc2023/day01/part1.clj
+++ src/aoc2023/day01/part1.clj
@@ -1,13 +1,38 @@
-(ns aoc2023.day01.part1)
-
-;; read file with input into string
-
-;; split each new line into separate string
+(ns aoc2023.day01.part1
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io]))
 
 ;; traverse each character in string and check if it is a numher using regex
+;; if character is a number put into a atom-vector
+(defn extract-nums
+  [str-input]
+  (def result (atom []))
 
-;; if character is a number put into a vector
+  (doseq [element (str/split str-input #"")]
+    (if (nil?  (re-matches #"[0-9]" element))
+      nil
+      (swap! result conj element)
+      )
+    )
+  result
+  )
 
-;; get the first and last number from each vector
+;; get the first and last number from each vector and form an integer
+(defn extract-firt-and-last [vect]
+  (Integer/parseInt (str/join (conj [] (first @vect)  (last @vect))))
+  )
 
-;; sum up the values of all elements in the vector of numbers
+(defn calculate
+  []
+  ;; read file with input into string
+  (def file-str (slurp "/home/k0sa7a/code/k0sa7a/aoc2023/src/aoc2023/day01/input1.csv"))
+  ;; split each new line into separate string
+  (def split-by-line (str/split-lines file-str))
+
+
+  (def result (reduce + (map extract-firt-and-last (map extract-nums split-by-line))))
+  (println "Result is:" result)
+  result
+  )
+
+(calculate)
