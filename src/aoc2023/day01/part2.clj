da03ba662be86b741699b4b5549d39c20cde3b77
Stefan Filev <stefan.s.filev@gmail.com>
2023-12-01 21:11:31 +0000

    some progress on checking digits that are words from the string

--- src/aoc2023/day01/part2.clj
+++ src/aoc2023/day01/part2.clj
@@ -2,43 +2,57 @@
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as io]))
 
-(def test1 "two1nine")
+(def test1 "eightwothree")
 
-(def digits [#"^one" #"two" #"three" #"four" #"five" #"six" #"seven" #"eight" #"nine"])
+(def digits ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
+(def digits-vals {"one" 1, "two" 2, "three" 3, "four" 4, "five" 5, "six" 6, "seven" 7, "eight" 8, "nine" 9})
+
+
 
 (str/index-of test1 "nine")
 
+;; helper function to get the integers of the string representations of the digits
+(defn transform-digits
+  [input-set]
+  (def accumulated (atom []))
+
+  (doseq [digit-hash input-set]
+
+    (let [[k v] (first digit-hash)]
+      (swap! accumulated conj {k (get digits-vals v)})))
+  @accumulated)
+
+;; checking the starting positions and values (represented as strings still) of digits within a string
 (defn check-digits
   [input-string]
-
-  (loop [i 0]
-    (when (< i (count input-string))
-      ;; (println (nth input-string i))
+  (def accumulated (atom []))
+  (doseq [digit (seq digits)]
 
 
 
 
-      (recur (inc i)))
+    (loop [i 0]
+      (when (< i (count input-string))
+        (let [index (str/index-of input-string digit i)]
+          ;; (println "i is" i)
+          ;; (println "digit is" digit)
+          ;; (println "index is" index)
+          ;; (println "accumulated is" @accumulated)
 
+
+
+          (if (not (nil? index))
+            (swap! accumulated conj {index digit}))
+
+          (recur (inc i))))))
+
+
+
+  (transform-digits (into #{} @accumulated))
   )
-)
+
+
+
 
 
 (check-digits test1)
- (re-find #"^two" "two1nine")
-
-(loop [i 0]
-  (when (< i (count "two1nine"))
-    (println "-------")
-      (println (nth "two1nine" i))
-
-    ;; (println (str/index-of "two1nine" "nine" i))
-
-    (let [index (str/index-of "two1ninetwo" "two" i)]
-      (println "i is" i)
-      (println "index is" index)
-
-      )
-
-
-    (recur (inc i))))
