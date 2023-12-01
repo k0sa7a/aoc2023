fdc584fb6d162cee098fdafc3661a1e10e284fe8
Stefan Filev <stefan.s.filev@gmail.com>
2023-12-01 11:03:58 +0000

    pseudo code for part1

--- src/aoc2023/day01/part1.clj
+++ src/aoc2023/day01/part1.clj
@@ -1,1 +1,13 @@
 (ns aoc2023.day01.part1)
+
+;; read file with input into string
+
+;; split each new line into separate string
+
+;; traverse each character in string and check if it is a numher using regex
+
+;; if character is a number put into a vector
+
+;; get the first and last number from each vector
+
+;; sum up the values of all elements in the vector of numbers
