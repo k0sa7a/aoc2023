3d522b36e4115abaeb67103b8c67eff85a187693
Stefan Filev <stefan.s.filev@gmail.com>
2023-12-02 11:49:20 +0000

    some initial ideas

---
+++ src/aoc2023/day02/part1.clj
@@ -0,0 +1,42 @@
+(ns aoc2023.day02.part1
+  (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io]))
+
+;; max values for each cube color
+(def max-cubes {"red" 12 "green" 13 "blue" 14 })
+
+;; game example (1 row) for testing purposes
+(def game1 "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+
+;; function to extract the game num from the game-row string
+(defn game-row-parser [game-row]
+
+  (let [split-result (str/split game-row #":")
+        game-name-string (first split-result)]
+
+
+    (->> game-name-string
+        (re-find #"[0-9]+")
+        (Integer/parseInt))
+    )
+  )
+
+;; function to extract the game parts from the game-row string
+(defn game-steps-parser [game-row]
+
+  (let [split-result (str/split game-row #":")
+        game-string (str/trim (last split-result))
+        game-steps (map str/trim (str/split game-string #";"))
+        game-steps-with-elements (map #(str/split % #",") game-steps)
+        ;; game-steps-mappified (map #(hash-map (last %) (first %)) game-steps-with-elements)
+
+        ]
+
+     (doseq [game-step game-steps-with-elements]
+       (println game-step)
+
+       )
+
+    ))
+
+(game-steps-parser game1)
