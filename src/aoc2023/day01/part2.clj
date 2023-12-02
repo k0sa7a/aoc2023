51c7f843ad9b4719e1adf98caba1dba559a06859
Stefan Filev <stefan.s.filev@gmail.com>
2023-12-02 00:00:46 +0000

    some cleanup

--- src/aoc2023/day01/part2.clj
+++ src/aoc2023/day01/part2.clj
@@ -2,15 +2,11 @@
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as io]))
 
-(def test1 "two1nine")
-
 (def digits ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 (def digits-vals {"one" 1, "two" 2, "three" 3, "four" 4, "five" 5, "six" 6, "seven" 7, "eight" 8, "nine" 9})
 
 (def numbers ["1" "2" "3" "4" "5" "6" "7" "8" "9"])
 (def numbers-vals {"1" 1, "2" 2, "3" 3, "4" 4, "5" 5, "6" 6, "7" 7, "8" 8, "9" 9})
-
-(str/index-of test1 "nine")
 
 ;; helper function to transform the string number values into integers
 (defn transform-digits
@@ -23,16 +19,11 @@
       (swap! accumulated conj {k (get hash-map-for-check v)})))
   @accumulated)
 
-
-
 ;; checking the starting index and values (represented as strings still) of digits (numbers and substrings) within a string
 (defn check-digits
   [input-string sequence-for-check hash-map-for-check]
   (def accumulated (atom []))
   (doseq [digit (seq sequence-for-check)]
-
-
-
 
     (loop [i 0]
       (when (< i (count input-string))
@@ -43,11 +34,8 @@
 
           (recur (inc i))))))
 
-
-
   (transform-digits (into #{} @accumulated) hash-map-for-check)
   )
-
 
 ;; extract both word and integer digits from a string and get the final integer from first and last digit
 (defn get-first-last-digit [input-string]
@@ -55,7 +43,6 @@
   (def step1 (into [] (check-digits input-string digits digits-vals)))
   (def step2 (into step1 (check-digits input-string numbers numbers-vals)))
   (def step3 (into [] (sort-by first step2)))
-  (println step2)
 
 
   ;; (apply max (apply concat (map keys step2)))
@@ -72,15 +59,11 @@
          (map keys)
          (apply concat)
          (apply min)))
-  (println "min index is" min-ind)
-  (println "max index is" max-ind)
+  ;; (println "min index is" min-ind)
+  ;; (println "max index is" max-ind)
 
   (doseq [map-with-numbers step3]
-    (println "Map with numbers:" map-with-numbers)
-    ;; (println "MIN:" (get map-with-numbers min-ind))
-    ;; (println "MAX:" (get map-with-numbers max-ind))
-
-
+    ;; (println "Map with numbers:" map-with-numbers)
 
     (if (contains? map-with-numbers min-ind)
       (swap! result conj (map-with-numbers min-ind)))
@@ -106,6 +89,4 @@
   result
   )
 
-;; (get-first-last-digit "7pqrstsixteen")
 (calculate)
-;; (map get-first-last-digit ["two1nine" "eightwothree" "abcone2threexyz" "xtwone3four" "4nineeightseven2" "zoneight234" "7pqrstsixteen"])
